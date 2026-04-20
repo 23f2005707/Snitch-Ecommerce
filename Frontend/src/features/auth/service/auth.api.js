adacd6d31528720f6e89fc1fcb5c0a4d6aa74de5
Himanshu <23f2005707@ds.study.iitm.ac.in>
2026-04-20 19:48:45 +0530

    authentication setup

---
+++ Frontend/src/features/auth/service/auth.api.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const authApiInstance = axios.create({
+    baseURL: "http://localhost:3000/api/auth",
+    withCredentials: true,
+})
+
+
+export async function register({ fullname, email, contact, password }) {
+
+    const response = await authApiInstance.post("/register", {
+        email, fullname, contact, password
+    })
+    return response.data;
+}
